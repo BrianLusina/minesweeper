14d8131e057f55d00335efa99d921a84edd08a52
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-06-17 13:02:34 +0300

    Board tests
    Addition of board tests and new
    Chai and faker dependency addtions for testing.

---
+++ __tests__/Board.test.js
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+    render
+} from "react-dom";
+import Board from '../src/Board';
+import {
+    mount,
+    shallow
+} from "enzyme";
+import sinon from "sinon";
+import faker from "faker";
+import { assert } from "chai";
+
+
+describe('Board', () => {
+    let mountWrapper;
+    let shallowWrapper;
+
+    let props = {
+        mines: faker.random.number(10),
+        height: faker.random.number(10),
+        width: faker.random.number(10)
+    }
+
+    beforeEach(() => {
+        mountWrapper = mount(<Board {...props}/>);
+        shallowWrapper = shallow(<Board {...props}/>);
+    });
+
+    it("should be able to render", () => {
+        render(<Board {...props}/>, document.createElement("div"))
+    });
+
+    it("should be able to mount", () => {
+        mount(<Board {...props}/>)
+    });
+
+    it("should call static method getDerivedStateFromProps to update state", () => {
+        sinon.stub(Board, "getDerivedStateFromProps").callsFake(() => (props));
+        
+        // receive props
+        shallowWrapper.setProps(props);
+
+        expect(Board.getDerivedStateFromProps.calledOnce).toEqual(true);
+        expect(Board.getDerivedStateFromProps.returnValues[0]).toEqual(props);
+        expect(shallowWrapper.state().mines).toEqual(props.mines);
+        expect(shallowWrapper.state().height).toEqual(props.height);
+        expect(shallowWrapper.state().width).toEqual(props.width);
+    });
+
+    it("should call componentDidMount once and call __initBoardData", () => {
+        sinon.spy(Board.prototype, "componentDidMount");
+        sinon.spy(Board.prototype, "__initBoardData");
+
+        mount(<Board {...props}/>)
+
+        expect(Board.prototype.componentDidMount.calledOnce).toEqual(true);
+        expect(Board.prototype.__initBoardData.calledOnce).toEqual(true);
+    });
+
+    it("should call __initBoardData instance method and call __createEmptyArray, __plantMines", () => {
+        mountWrapper.instance().__createEmptyArray = jest.fn();
+        mountWrapper.instance().__plantMines = jest.fn();
+        mountWrapper.instance().render()
+
+        mountWrapper.instance().__initBoardData();
+
+        expect(mountWrapper.instance().__createEmptyArray).toHaveBeenCalledTimes(1);
+        //expect(mountWrapper.instance().__plantMines).toHaveBeenCalledTimes(1);
+
+        // expect(mountWrapper.state("boardData")).toEqual();
+    });
+
+    it("should call __getRandomNumber that always returns a number", () => {
+        let dimension = faker.random.number(10)
+        let randomNum = mountWrapper.instance().__getRandomNumber(dimension);
+        assert.isNumber(randomNum)
+    });
+
+})
--- src/Board.jsx
+++ src/Board.jsx
@@ -2,19 +2,117 @@ import React, { Component } from 'react';
 import { number } from "prop-types";
 import "./styles/board.scss";
 
-
+/**
+ * Board component is responsible for rendering the board is is aware of the game status:
+ * Number of mines, yet to be found, whether the game has been won or not and knows which cell
+ * has a mine or not
+ */
 class Board extends Component {
     constructor(props){
         super(props);
         this.state = {
             width: 0,
             height: 0,
-            mines: 0
+            mines: 0,
+            boardData: []
+        }
+
+        this.__initBoardData = this.__initBoardData.bind(this);
+    }
+
+    /**
+     * Initialize board data on mount and set the boardData
+     */
+    __initBoardData(){
+        const { height, width, mines } = this.props;
+        let boardData = this.__createEmptyArray(height, width);
+        //boardData = this.__plantMines(boardData, height, width, mines);
+
+        this.setState({
+            boardData
+        })
+    }
+
+    /**
+     * Creates a 2D array and each cell data[x][y] with default attributes
+     * @param {Number} height height of the board
+     * @param {Number} width width of the board
+     * @returns {Array}
+     */
+    __createEmptyArray = (height, width) => {
+        let data = [];
+        for (let i = 0; i < height; i++) {
+            data.push([]);
+            for (let j = 0; j < width; j++) {
+                data[i][j] = {
+                    x: i,
+                    y: j,
+                    isMine: false,
+                    neighbour: 0,
+                    isRevealed: false,
+                    isEmpty: false,
+                    isFlagged: false,
+                };
+            }
+        }
+        return data;
+    }
+
+    /**
+     * Randomly plants mines on the board and assigns the cells with mines an attribute of isMine
+     * @param {Array} data Initial Array of cells,
+     * @param {Number} height Height of the board
+     * @param {Number} width width of the board
+     * @param {Number} mines Number of mines to plant
+     * @returns {Array} Returns the array cells, with randomly planted mines
+     */
+    __plantMines = (data, height, width, mines) => {
+        let randomX, randomY, minesPlanted = 0;
+
+        while(minesPlanted < mines){
+            randomX = this.__getRandomNumber(width);
+            randomY = this.__getRandomNumber(height); 
+            console.log(data[0][0])
+            let cell = data[randomX][randomY];
+            
+            // if this is not a mine
+            if(!cell.isMine){
+                cell.isMine = true
+                minesPlanted += 1
+            }
+        }
+        return data;
+    }
+
+    /**
+     * Get a random number given a dimenstion
+     * @param {Number} dimension
+     */
+    __getRandomNumber = dimension => {
+        return Math.floor((Math.random() * 1000) + 1) % dimension;
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState){
+        const {  mines, height, width } = nextProps;
+
+        if (mines !== prevState.mines || height !== prevState.height || width !== prevState.width){
+            return {
+                ...prevState,
+                mines,
+                height,
+                width
+            }
+        } else {
+            return null
         }
     }
 
     render(){
         return <div/>
+    }
+
+    componentDidMount() {
+        this.__initBoardData()   
     }
 }
 
