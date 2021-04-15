9520e42a6ef255a51bb39f7888f282b5941c5c92
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-04-15 16:19:55 +0300

    chore(lint): fix linting issues :(

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,8 @@ import './index.scss';
 ReactDOM.render(
   <React.StrictMode>
     <Game />
-  </React.StrictMode>, 
-  document.getElementById('root')
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
