ae7eaaafaac158559b45645cddac462fe3c6d5af
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-04-15 09:44:59 +0300

    chore(scripts): move scripts

--- scripts/build_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Change this to match your docker registry
-REGISTRY=${DOCKER_REGISTRY:-thelusina}
-IMAGE=${DOCKER_IMAGE:-minesweeper}
-TAG=${DOCKER_TAG:-latest}
-
-docker build -t "${REGISTRY}"/"${IMAGE}":"${TAG}" .
--- scripts/run_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-./build_docker.sh
-
-docker images
-
-# shellcheck disable=SC2086
-docker run -p 8080:80 --name=minesweeper ${REGISTRY}/"${IMAGE}":"${TAG}"
