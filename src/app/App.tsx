0ae2b19123b77ac69c63b06587954887cab11de3
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-06-19 14:54:48 +0300

    feat(components): header and footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
-import logo from '@images/logo.svg';
+import Header from '@sections/Header';
+import Footer from '@sections/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <Header />
+      <div id='main'>
+        
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/sections/Header/Header.tsx
@@ -0,0 +1,19 @@
+import { FunctionComponent } from "react";
+
+const Header: FunctionComponent = () => {
+    return (
+        <header id="header">
+        <div className="inner">
+            <a href="#" className="image avatar">
+                <img src="images/avatar.jpg" alt="" />
+            </a>
+            <h1><strong>I am Strata</strong>, a super simple<br />
+            responsive site template freebie<br />
+            crafted by <a href="http://html5up.net">HTML5 UP</a>.
+            </h1>
+        </div>
+        </header>
+    )
+};
+
+export default Header;
